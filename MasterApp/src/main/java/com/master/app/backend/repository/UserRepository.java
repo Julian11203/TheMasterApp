69c598438409bf19de2a01823f932a54fce49b00
Julian Guzman <126201443+Julian11203@users.noreply.github.com>
2024-09-08 22:02:47 -0500

    ...

--- MasterApp/src/main/java/com/master/app/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.master.app.backend.repository;
-
-import com.master.app.backend.persistence.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
